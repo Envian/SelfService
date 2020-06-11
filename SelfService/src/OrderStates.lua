2b6591dceaff72990bf22fdb922f0823a98b2655
mcscurvy <31036123+mcscurvy@users.noreply.github.com>
2020-06-10 23:15:58 -0700

    Final Commit
    Maybe works now

--- SelfService/src/OrderStates.lua
+++ SelfService/src/OrderStates.lua
@@ -88,11 +88,14 @@ ns.OrderStates = {
 			return ns.OrderStates.ORDER_PLACED;
 		end,
 		TRADE_COMPLETED = function(customer)
-			customer:whisper(ns.L.enUS.CRAFTING_ORDER);
-			ns.ActionQueue.clearTradeAction();
 
 			if customer.CurrentOrder:isTradeCompletable() then
+				customer:whisper(ns.L.enUS.CRAFTING_ORDER);
+				ns.ActionQueue.clearTradeAction();
 				return ns.OrderStates.CRAFT_ORDER;
+			else
+				ns.ActionQueue.clearTradeAction();
+				return ns.OrderStates.ORDER_PLACED;
 			end
 		end
 	}),
